d8632a48ec0b9b6fefdfa813e43c06018a3a628c
IlyaOrluk <misfits120@mail.ru>
2019-11-19 20:54:58 +0300

    Updates

---
+++ precache-manifest.272194af0c32cfe80399d8dbb12526c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2d95412e34ed2c25b7135025c4f95ec",
+    "url": "/IlyaOrluk/Game-store/static/media/bg2.e2d95412.jpg"
+  },
+  {
+    "revision": "076ae5b7d48a1da6aa09",
+    "url": "/IlyaOrluk/Game-store/static/js/runtime~main.076ae5b7.js"
+  },
+  {
+    "revision": "8110af42fc4adbc3da55",
+    "url": "/IlyaOrluk/Game-store/static/js/main.8110af42.chunk.js"
+  },
+  {
+    "revision": "6120bbe708dd00797f8d",
+    "url": "/IlyaOrluk/Game-store/static/js/1.6120bbe7.chunk.js"
+  },
+  {
+    "revision": "8110af42fc4adbc3da55",
+    "url": "/IlyaOrluk/Game-store/static/css/main.640acef8.chunk.css"
+  },
+  {
+    "revision": "0e49cbfa702460285fadd9e1e9dd6aa5",
+    "url": "/IlyaOrluk/Game-store/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Game-store/precache-manifest.1fedf2a88a7679b8e05f79b2ef2e0b1f.js"
+  "/IlyaOrluk/Game-store/precache-manifest.272194af0c32cfe80399d8dbb12526c7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Game-store/index.html", {
+workbox.routing.registerNavigationRoute("/IlyaOrluk/Game-store/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
